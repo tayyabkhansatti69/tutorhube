2f9c7600e060c4657408772f0ef0d10770c9212e
Tayyab Khan satti129 <90499823+tayyabksatti@users.noreply.github.com>
2024-09-28 15:53:06 +0500

    code pushed by tayyab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
 "use client";
 
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 // import SaleFooter from "../layouts/saleSite/footer/sale-footer";
 // import SaleHeader from "../layouts/saleSite/header/Sale-header";
 // import MainSaleSite from "../sections/salesite";
-import SignIn from "./(auth)/sign-in/page";
+
 
 
 export default function Home() {
+  const router=useRouter()
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/sign-in');
+    }, 0); // Delay by 0 ms to ensure the router is mounted
+    
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, [router]);
   return (
     <>
-      {/* <SaleHeader /> */}
-      {/* <MainSaleSite /> */}
-      {/* <SaleFooter/> */}
-      <SignIn/>
+      {/* <SaleHeader />
+      <MainSaleSite />
+      <SaleFooter/> */}
+      
+
     </>
   );
 }
